be4452475ec0c6c70523eb73bc7af02e0dfc460e
ula <zuma9@yandex.ru>
2022-08-12 02:24:23 +0300

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,27 @@ console.log(`popup`, popup);
 console.log(`openButton`, openButton);
 console.log(`closeButton`, closeButton);
 
-// openButton.addEventListener('click', function () {
-//   console.log('hi callback');
-// })
-
+openButton.addEventListener('click', function () {
+  console.log('hi callback');
+} )
 
 const togglePopup = function () {
   popup.classList.toggle('popup_opened');
 }
 
-openButton.addEventListener('click', togglePopup)
-closeButton.addEventListener('click', togglePopup)
+// openButton.addEventListener('click', togglePopup)
+// closeButton.addEventListener('click', togglePopup)
+// let popup = document.querySelector('.popup');
+// let openButton = document.querySelector('.profile__edit-button');
+// let closeButton = popup.querySelector('.popup__close');
+//
+// let addPopup = function () {
+//   popup.classList.add('popup_opened');
+// }
+//
+// let removePopup = function () {
+//   popup.classList.remove('popup_opened');
+// }
+//
+// openButton.addEventListener('click', removePopup);
+// closeButton.addEventListener('click', addPopup);
